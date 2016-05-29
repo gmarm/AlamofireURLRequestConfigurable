21dc10a2688d473c466b959f24ec743f5405ff47
George Marmaridis <gmarmas@gmail.com>
2016-05-29 10:21:42 +0200

    Added logic for loading the API key from a .plist file.

--- Example/AlamofireURLRequestConfigurable/APIConfiguration.swift
+++ Example/AlamofireURLRequestConfigurable/APIConfiguration.swift
@@ -10,12 +10,13 @@ import Foundation
 
 struct APIConfiguration {
     static let baseURLString = "http://www.giantbomb.com/api"
-    static let defaultParamaters = ["api_key": "b317c494a1ca7de307",
+    static let defaultParamaters = ["api_key": APIConfiguration.APIKey(),
                              "format": "json",]
     static let headers = ["X-Custom-Header-1": "Value1",
                    "X-Custom-Header-2": "Value2"]
 }
 
+// MARK: Helpers
 extension APIConfiguration {
     static func URLString(path: String) -> String {
         return "\(baseURLString)\(path)"
@@ -27,5 +28,22 @@ extension APIConfiguration {
             parameters[key] = value
         }
         return parameters
+    }
+}
+
+// MARK: API Key
+extension APIConfiguration {
+    static private func APIKey() -> String? {
+        guard let path = NSBundle.mainBundle().pathForResource("API", ofType: "plist") else {
+            print("You need your own API key to use the example - you can create one here: http://www.giantbomb.com/api/")
+            return nil
+        }
+        guard let APIPlistDictionary = NSDictionary(contentsOfFile: path) else {
+            return nil
+        }
+        guard let APIKey = APIPlistDictionary["API_KEY"] else {
+            return nil
+        }
+        return APIKey as? String
     }
 }
