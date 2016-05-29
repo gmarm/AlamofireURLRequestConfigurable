8dec104b7471d3d642d6611e8580c702c99839e2
George Marmaridis <gmarmas@gmail.com>
2016-05-29 20:13:46 +0200

    Removed API plist logic so example compiles normally when repo is cloned.

--- Example/AlamofireURLRequestConfigurable/APIConfiguration.swift
+++ Example/AlamofireURLRequestConfigurable/APIConfiguration.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct APIConfiguration {
     static let baseURLString = "http://www.giantbomb.com/api"
-    static let defaultParamaters = ["api_key": APIConfiguration.APIKey(),
+    static let defaultParamaters = ["api_key": "<YOUR_API_KEY_HERE>",
                              "format": "json",]
     static let headers = ["X-Custom-Header-1": "Value1",
                    "X-Custom-Header-2": "Value2"]
@@ -28,22 +28,5 @@ extension APIConfiguration {
             parameters[key] = value
         }
         return parameters
-    }
-}
-
-// MARK: API Key
-extension APIConfiguration {
-    static private func APIKey() -> String? {
-        guard let path = NSBundle.mainBundle().pathForResource("API", ofType: "plist") else {
-            print("You need your own API key to use the example - you can create one here: http://www.giantbomb.com/api/")
-            return nil
-        }
-        guard let APIPlistDictionary = NSDictionary(contentsOfFile: path) else {
-            return nil
-        }
-        guard let APIKey = APIPlistDictionary["API_KEY"] else {
-            return nil
-        }
-        return APIKey as? String
     }
 }
